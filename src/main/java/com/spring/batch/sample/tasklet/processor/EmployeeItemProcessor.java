57ba9cc8541930171923a5c504a21a7a54239d74
Shannon Maret <sjmaret@gmail.com>
2021-09-08 16:02:44 -0500

    Add files via upload

---
+++ src/main/java/com/spring/batch/sample/tasklet/processor/EmployeeItemProcessor.java
@@ -0,0 +1,25 @@
+package com.spring.batch.sample.tasklet.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.spring.batch.sample.tasklet.model.Employee;
+import com.spring.batch.sample.tasklet.model.Profile;
+
+public class EmployeeItemProcessor implements ItemProcessor<Employee, Profile> {
+
+	@Override
+    public Profile process(final Employee emp) throws Exception {
+	
+		String profileName = "";
+        if (emp.getExpInYears() < 5) {
+        	profileName = "Developer";
+        } else if (emp.getExpInYears() >= 5 && emp.getExpInYears() <= 8) {
+        	profileName = "Team Lead";
+        } else if (emp.getExpInYears() > 8) {
+        	profileName = "Manager";
+        }
+        System.out.println("Emp Code: " + emp.getEmpCode() + 
+        		", Emp Name: " + emp.getEmpName() + ", Profile Name:" + profileName);
+	return new Profile(emp.getEmpCode(), emp.getEmpName(), profileName);
+    }
+}
